2b86d34de65f91b81d2fb02b9bb522506acdbd6b
William S Ramos <wsampaio@minsait.com>
2024-11-12 17:16:45 -0300

    Projeto Spring Criado

---
+++ src/main/java/br/com/learning/pokedexapi/PokedexapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.learning.pokedexapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedexapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/learning/pokedexapi/PokedexapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.learning.pokedexapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
